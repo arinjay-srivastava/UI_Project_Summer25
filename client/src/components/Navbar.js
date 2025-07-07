f61f52a2d3cb3b7c1cf4abc7c2b3c5533b0ca712
arinjay-srivastava <yashshir7@gmail.com>
2025-07-07 19:37:27 +0530

    Add featiure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    navigate('/');
+  };
+
+  const isLoggedIn = localStorage.getItem('user');
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">NotePad</Link>
-        <div className="navbar-nav">
-          <Link className="nav-link" to="/profile">Profile</Link>
-          <Link className="nav-link" to="/">Login</Link>
-          <Link className="nav-link" to="/register">Register</Link>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
+      <Link className="navbar-brand" to="/">NotePad</Link>
+      <div className="navbar-nav ms-auto">
+        {isLoggedIn ? (
+          <>
+            <Link className="nav-link" to="/profile">Profile</Link>
+            <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link className="nav-link" to="/">Login</Link>
+            <Link className="nav-link" to="/register">Register</Link>
+          </>
+        )}
       </div>
     </nav>
   );
